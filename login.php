6a27105db9459f0fccf846dbf89bc475ccc1f7aa
Yap Hao Jay <yaphaojay1202@e.newera.edu.my>
2023-08-17 18:07:11 +0800

    Update

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DFS</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+
+</head>
+<body>
+<section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-white text-dark" style="border-top-right-radius: 270px 200px; border-top-left-radius: 270px 200px;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-5 mt-5 text-uppercase">Log In</h2>
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" class="form-control form-control-lg border border-3" placeholder="Username/Email" />
+                
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" class="form-control form-control-lg border border-3" placeholder="Password" autocomplete="off"/>
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-black-50" href="#!">Forgot password?</a></p>
+
+              <button class="btn btn-outline-dark btn-lg px-5" type="submit">Log In</button>
+
+            </div>
+
+            <div>
+              <p class="mb-0">Don't have an account? <a href="register.php" class="text-black-50 fw-bold">Sign Up</a>
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DFS</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/register.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+
+</head>
+<body>
+<section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100">
+      <div class="col-md-8 col-lg-7 col-xl-6">
+        <div class="border rounded-pill border-secondary bg-white text-dark d-flex justify-content-center align-items-center" style="block-size:300px">
+        <div class="text-center">
+            <h1 class="lh-lg text-uppercase">Welcome To DFS</h1>
+            <p class="lh-lg fs-5">Store Your Documents with us!</p>
+        </div>
+        
+        </div>
+      </div>
+      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        <form>
+            <h2>REGISTER AN ACCOUNT NOW!</h2>
+            <div class="divider d-flex align-items-center my-4">
+          </div>
+        
+          <div class="form-outline mb-4">
+            <input type="text" class="form-control form-control-lg" placeholder="Name"/>
+          </div>
+          <div class="form-outline mb-4 d-flex">
+            <input type="email" class="form-control form-control-lg" placeholder="Email"/>
+            <button type="submit" class="btn btn-white btn-outline-dark btn-lg btn-block d-flex">Verify</button>
+          </div>
+          <div class="form-outline mb-4 d-flex">
+            <input type="tel" class="form-control form-control-lg" placeholder="Phone Number"/>
+            <button type="submit" class="btn btn-white btn-outline-dark btn-lg btn-block d-flex">Verify</button>
+          </div>
+     
+          <div class="form-outline mb-4">
+            <input type="password" class="form-control form-control-lg" placeholder="Password" autocomplete="off"/>
+          </div>
+          <div class="form-outline mb-4">
+            <input type="password" class="form-control form-control-lg" placeholder="Re-enter Password" autocomplete="off"/>
+          </div>
+
+          <div class="d-flex justify-content-around align-items-center mb-4">
+  
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+              <label class="form-check-label" for="form1Example3">I agree with the term and conditions stated.</label>
+            </div>
+            <button type="submit" class="btn btn-white btn-outline-dark btn-lg btn-block">Register</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
